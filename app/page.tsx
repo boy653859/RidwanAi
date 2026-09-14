51cb1ef01bfaba778a40a53c0707442c93e22c76
rrr_181489 <yt181489@gmail.com>
2026-09-14 21:16:03 +0600

    Update voice identifiers in VOICES constant

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@
 import { useState } from "react";
 
 const VOICES = {
-  Female: "59ba7dee-8f9a-432f-a6c0-ffb33666b654",
+  Ridwan: "89da9de1-fa23-4598-a1d2-481817edd69a",
+  Sadik: "1d87a960-7d27-46fe-8498-7828360fea82",
   Male: "2ba861ea-7cdc-43d1-8608-4045b5a41de5",
-  Azhari: "89da9de1-fa23-4598-a1d2-481817edd69a",
+  Female: "59ba7dee-8f9a-432f-a6c0-ffb33666b654",
 } as const;
 
 const models = [
